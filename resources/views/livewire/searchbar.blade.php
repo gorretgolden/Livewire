69a9b1c25becb4e1b903b1424cbc585975c55a16
gorret golden <nabatanzigorret143@gmail.com>
2024-05-08 19:05:36 +0300

    updated searchbar

--- resources/views/livewire/searchbar.blade.php
+++ resources/views/livewire/searchbar.blade.php
@@ -6,10 +6,10 @@
 
 
 
-        <form wire:model.live='search' class="d-flex" role="search" wire:submit="searchUsers">
+        <form wire:model.live='search' class="d-flex" role="search">
 
             <input wire:model='name' class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success" type="submit">Search</button>
+            {{-- <button class="btn btn-outline-success" type="submit">Search</button> --}}
         </form>
 
         @if (sizeOf($users) > 0)
