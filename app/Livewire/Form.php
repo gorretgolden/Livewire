26ec98ec19ecf5213ffdef1e35952b6c16b02b0d
gorret golden <nabatanzigorret143@gmail.com>
2024-05-08 17:19:36 +0300

    validation

--- app/Livewire/Form.php
+++ app/Livewire/Form.php
@@ -7,11 +7,17 @@ use App\Models\User;
 
 class Form extends Component
 {
-    public $name = 'name';
-    public $email = 'email';
-    public $password = 'password';
+    public $name = ' ';
+    public $email = ' ';
+    public $password = ' ';
 
     public function createNewUser(){
+
+        $this->validate([
+            'name'=>'required|string|min:5',
+            'email'=>'required|email|unique:users',
+            'password'=>'required|password|min:5,max:8'
+        ]);
         User::create([
             'name'=>$this->name,
             'email'=> $this->email,
--- resources/views/livewire/form.blade.php
+++ resources/views/livewire/form.blade.php
@@ -1,11 +1,23 @@
-<div>
+<div class=" p-5 container">
     {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
     <form wire:submit="createNewUser">
+
         <input type="email" wire:model="email" placeholder="email">
+        @error('email')
+            <span class="text-danger">{{ $message }}</span>
+        @enderror
+
         <br>
-        <input type="text"  wire:model="name" placeholder="name">
+        <input type="text" wire:model="name" placeholder="name">
+        @error('name')
+            <span class="text-danger">{{ $message }}</span>
+        @enderror
         <br>
-        <input type="password"  wire:model="password" placeholder="esoptron">
-        <button >Create</button>
+        <input type="password" wire:model="password" placeholder="esoptron">
+        @error('password')
+            <span class="text-danger">{{ $message }}</span>
+        @enderror
+        <br>
+        <button class="btn btn-primary">Create</button>
     </form>
 </div>
