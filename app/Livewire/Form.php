b52ac3d505b781c4c0b258c93abb8703a9f10af1
gorret golden <nabatanzigorret143@gmail.com>
2024-05-08 17:54:39 +0300

    added flash messages

--- app/Livewire/Form.php
+++ app/Livewire/Form.php
@@ -7,22 +7,30 @@ use App\Models\User;
 
 class Form extends Component
 {
-    public $name = ' ';
-    public $email = ' ';
-    public $password = ' ';
+    
+    public $name ;
+    public $email ;
+    public $password ;
 
     public function createNewUser(){
 
         $this->validate([
             'name'=>'required|string|min:5',
             'email'=>'required|email|unique:users',
-            'password'=>'required|password|min:5,max:8'
+            'password'=>'required|min:5,max:8'
         ]);
         User::create([
             'name'=>$this->name,
             'email'=> $this->email,
             'password'=>$this->password
         ]);
+
+        #reset form
+        $this->reset(['name','email','password']);
+
+        #flash message
+        request()->session()->flash('success','You registered successfully');
+
         
     }
 
--- resources/views/livewire/form.blade.php
+++ resources/views/livewire/form.blade.php
@@ -1,23 +1,42 @@
 <div class=" p-5 container">
     {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
+
+
     <form wire:submit="createNewUser">
+        @if (session('success'))
+        <span class=""></span>
+        <div class="shadow-sm p-3 mb-5 text-white bg-success rounded">{{ session('success') }}</div>
+            
+        @endif
 
-        <input type="email" wire:model="email" placeholder="email">
-        @error('email')
-            <span class="text-danger">{{ $message }}</span>
-        @enderror
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Name</label>
+            <input type="text" wire:model='name' class="form-control" placeholder="Enter your name">
+        
+            @error('name')
+                <span class="text-danger">{{ $message }}</span>
+            @enderror
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Email address</label>
+            <input type="email" wire:model='email' class="form-control" id="exampleInputEmail1"
+                aria-describedby="emailHelp">
+            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
 
-        <br>
-        <input type="text" wire:model="name" placeholder="name">
-        @error('name')
-            <span class="text-danger">{{ $message }}</span>
-        @enderror
-        <br>
-        <input type="password" wire:model="password" placeholder="esoptron">
-        @error('password')
-            <span class="text-danger">{{ $message }}</span>
-        @enderror
-        <br>
-        <button class="btn btn-primary">Create</button>
+            @error('email')
+                <span class="text-danger">{{ $message }}</span>
+            @enderror
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Password</label>
+            <input type="password" wire:model='password' class="form-control" id="exampleInputPassword1">
+            @error('password')
+                <span class="text-danger">{{ $message }}</span>
+            @enderror
+
+        </div>
+
+        <button type="submit" class="btn btn-primary">Submit</button>
     </form>
+
 </div>
